ea80d775231001c44836c5c313d4ff753123d8a2
arcturas1 <arcturas1@gmail.com>
2017-09-17 13:36:11 -0600

    javadoc updates

--- src/main/java/FourBitTwoDisclosureDeviceUnlocker.java
+++ src/main/java/FourBitTwoDisclosureDeviceUnlocker.java
@@ -14,15 +14,6 @@
  */
 
 public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker {
-	
-	/*
-	 * Suppress Instantiation of objects from this utility class
-	 */
-	private FourBitTwoDisclosureDeviceUnlocker() {
-		
-	}
-	
-	
     /**
      *Unlocks a resource controlled by a 4-bit/2-disclosure device. Behavior is unspecified if parameter is not a reference to a valid 4-bit/2-disclosure device.
      *
@@ -31,14 +22,10 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker {
      */
     public static boolean unlock(Device dev){
         //Set up variables
-        boolean deviceUnlocked = false; //denotes whether the device is unlocked, assumed false
-        final int max = 100;		//The max number of spins per cycle of the heuristic
-        final int MAX_CYCLES = 3;		//The max number of unlocking attempts to be made
-        final CharSequence STANDARD_PATTERN = "??--"; // the typical peek pattern.
-        final CharSequence ALTERNATIVE_PATTERN = "--??"; // an alternative peek pattern.
-        CharSequence peekRequestPattern; //the pattern to peek from the device
-        CharSequence pokePattern; 		//the pattern to poke into the device
-        CharSequence peekResponse; 		//the partial state obtained by peeking into the device
+        boolean forSpin;
+        CharSequence forPeek = "??--";
+        CharSequence forPoke = "TT--";
+        CharSequence fromPeek;
 
         // Reset the trace
         FourBitTwoDisclosureDeviceUnlocker.log(null);
@@ -46,48 +33,31 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker {
         // We're staring the unlock
         FourBitTwoDisclosureDeviceUnlocker.log("Unlock start");
 
-        //Unlocking heuristic: 
-        	//loop halts after MAX_CYCLES iterations of MAX_SPINS attempts
-        	//to unlock the device, or when the device is unlocked.
-        unlockHeuristic:{//begin unlockHeuristic
-        	for(int j = 0; j < MAX_CYCLES; j ++) {
-            	
-        		if(j%2==0) { // if the cycle number is even, use the standard pattern for peek/poke
-        			peekRequestPattern = STANDARD_PATTERN;
-        			pokePattern = STANDARD_PATTERN.toString().replace('?', 'T');
-        		}else { //otherwise use the alternative pattern
-        			peekRequestPattern = ALTERNATIVE_PATTERN;
-        			pokePattern = ALTERNATIVE_PATTERN.toString().replace('?', 'T');
-        		}
-        			
-            	for(int i=0; i <= max; i++) {
-                    deviceUnlocked = dev.spin();
-                    
-                    //If the device is unlocked, log the state and exit the heuristic loop
-                    if (deviceUnlocked) {
-                        FourBitTwoDisclosureDeviceUnlocker.log("Spin: unlocked");
-                        break unlockHeuristic;
-                    }
+        //The loop is set to run 100 times before halting.
+        for(int i=0; i <= 100; i++) {
+            forSpin = dev.spin();
 
-                    FourBitTwoDisclosureDeviceUnlocker.log("Spin: locked");
-                    FourBitTwoDisclosureDeviceUnlocker.log("Peek pattern: " + peekRequestPattern.toString());
-
-                    peekResponse = dev.peek(peekRequestPattern);
-                    FourBitTwoDisclosureDeviceUnlocker.log("Peek result: " + peekResponse.toString());
-
-                    FourBitTwoDisclosureDeviceUnlocker.log("Poke: " + pokePattern.toString());
-                    dev.poke(pokePattern);
-
-                    FourBitTwoDisclosureDeviceUnlocker.log(dev.toString());
-
-                }
-            	
+            //If true, loop is exited
+            if (forSpin) {
+                FourBitTwoDisclosureDeviceUnlocker.log("Spin: unlocked");
+                FourBitTwoDisclosureDeviceUnlocker.log("Unlock SUCCESS");
+                return true;
             }
-        }//end unlockHeuristic
-        
-        FourBitTwoDisclosureDeviceUnlocker.log("Unlock Successful: " +  deviceUnlocked);
 
-        return deviceUnlocked;
+            FourBitTwoDisclosureDeviceUnlocker.log("Spin: locked");
+            FourBitTwoDisclosureDeviceUnlocker.log("Peek pattern: " + forPeek.toString());
+
+            fromPeek = dev.peek(forPeek);
+            FourBitTwoDisclosureDeviceUnlocker.log("Peek result: " + fromPeek.toString());
+
+            FourBitTwoDisclosureDeviceUnlocker.log("Poke: " + forPoke.toString());
+            dev.poke(forPoke);
+
+            FourBitTwoDisclosureDeviceUnlocker.log(dev.toString());
+
+        }
+        FourBitTwoDisclosureDeviceUnlocker.log("Unlock FAILED");
+        return false;
     }
 
 }
